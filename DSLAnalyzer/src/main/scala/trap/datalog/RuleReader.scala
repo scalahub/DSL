d7aa1216b408c423a3f0a3fdde82e7e58505c9de
ScalaHub <scalahub@gmail.com>
2019-05-15 04:57:22 +0530

    Cleaned up code

--- DSLAnalyzer/src/main/scala/trap/datalog/RuleReader.scala
+++ DSLAnalyzer/src/main/scala/trap/datalog/RuleReader.scala
@@ -1,17 +1,6 @@
 
 package trap.datalog
 
-import trap.file.Util._
-import trap.Util._
-//import org.apache.commons.io.IOUtils
-
 class RuleReader(initialRulesFile:String) {
-  println("Initial rules file: "+initialRulesFile)
-//  def streams = getStreams(initialRulesFile)
-//  def streamsIndex = streams.zipWithIndex
-//  def xdslRulesString = using(trycatch(streams)){is =>
-    //println("[Loading rules] from: "+is)
-//    IOUtils.toString(is, "UTF-8")      
-//  }
   def xdslRulesString = trap.file.Util.readTextFileToString(initialRulesFile)
 }
